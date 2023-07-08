ec57a56cf98e2e759555c6ac8c2cd0f9e785acd6
ZAFAR <muhammedzafar.mm@gmail.com>
2023-07-08 20:26:07 +0530

    [PATCH] MINOR

--- management/urls.py
+++ management/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import UserCreateView
 
 urlpatterns = [
+    path('', UserCreateView.as_view(), name='user-list'),
     path('<str:id>/', UserCreateView.as_view(), name='user-detail'),
 ]
