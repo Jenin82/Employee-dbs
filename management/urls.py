a260b54ebad2cb10e205ea0bd2f4dfc5f4d7e3b3
ZAFAR <muhammedzafar.mm@gmail.com>
2023-07-08 15:49:39 +0530

    [FEAT] Update requirements.txt

--- management/urls.py
+++ management/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserCreateView
 
 urlpatterns = [
-    path('users/', UserCreateView.as_view(), name='user-create'),
+    path('create/', UserCreateView.as_view(), name='user-create'),
 ]
